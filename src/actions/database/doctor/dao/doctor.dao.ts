a30c6e82800133e1d65581b4d04025ce6b659647
3illBaby <thrillanon@outlook.com>
2024-04-01 12:18:44 +0100

    chore(doctor.dao.ts): completed dao funcs for doctors

--- src/actions/database/doctor/dao/doctor.dao.ts
+++ src/actions/database/doctor/dao/doctor.dao.ts
@@ -1,1 +1,49 @@
 'use server'
+import { schemaProvider } from '@/actions/mongoose/schemas/schema_provider/schema.providers'
+import { ApprovalStatus, Category } from '@/actions/shared/global'
+import { CreateDoctorType } from '../interface/doctor.interface'
+
+export class DoctorDB {
+  static Doctor = schemaProvider.getSchema('Doctor')
+
+  static async createNewDoctor(doctor: CreateDoctorType) {
+    return await this.Doctor.create({
+      id: doctor.id,
+      name: doctor.name,
+      email: doctor.email,
+      profilePicture: doctor.profilePicture || '',
+      specialty: doctor.specialty,
+      location: doctor.location,
+      phoneNumber: doctor.phoneNumber,
+      walletAddress: doctor.walletAddress,
+      regNo: doctor.regNo,
+      numberOfApprovals: 0,
+      status: ApprovalStatus.Pending,
+      category: Category.Doctor,
+    })
+  }
+
+  static async fetchDoctor(id: number) {
+    return await this.Doctor.findOne({ id })
+  }
+
+  static async fetchDoctorByAddress(address: string) {
+    return await this.Doctor.findOne({ walletAddress: address })
+  }
+
+  static async fetchAllDoctors() {
+    return await this.Doctor.find()
+  }
+
+  static async fetchDoctorWithPendingStatus() {
+    return await this.Doctor.find({ status: ApprovalStatus.Pending })
+  }
+
+  static async fetchDoctorWithApprovedStatus() {
+    return await this.Doctor.find({ status: ApprovalStatus.Approved })
+  }
+
+  static async deleteDoctor(address: string) {
+    return await this.Doctor.deleteOne({ walletAddress: address })
+  }
+}
