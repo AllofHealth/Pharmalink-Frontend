e000f6fe51b99ed1d9b98e8353ed2fbcb5f0082c
3illBaby <thrillanon@outlook.com>
2024-04-01 10:55:37 +0100

    chore(schema.provider.ts): added Schemas to provider

--- src/actions/mongoose/schemas/schema_provider/schema.providers.ts
+++ src/actions/mongoose/schemas/schema_provider/schema.providers.ts
@@ -1,7 +1,15 @@
 'use server'
 
 import Admin from '../admin.schema'
+import Doctor from '../doctor.schema'
+import Hospital from '../hospital.schema'
+import Pharmacist from '../pharmacist.schema'
+import Patient from '../patient.schema'
 
 export const SchemaProvider = {
   Admin: Admin,
+  Doctor: Doctor,
+  Hospital: Hospital,
+  Pharmacist: Pharmacist,
+  Patient: Patient,
 }
