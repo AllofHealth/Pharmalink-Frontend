1c9dd0a3f71c7e462a5fc1f210f131d57e80324a
3illBaby <thrillanon@outlook.com>
2024-04-01 19:41:18 +0100

    feat(hospital,service.ts): added hospital func

--- src/actions/database/hospital/service/hospital.services.ts
+++ src/actions/database/hospital/service/hospital.services.ts
@@ -1,1 +1,184 @@
 'use server'
+
+import {
+  ApprovalStatus,
+  Category,
+  ErrorCodes,
+  HospitalError,
+} from '@/actions/shared/global'
+import {
+  HospitalType,
+  CreateHospitalType,
+  PreviewType,
+  ApprovePractitionerType,
+} from '../interface/hospital.interface'
+import { DatabaseProvider } from '../../providers/db.providers'
+
+class HospitalHelpers {
+  static DB = DatabaseProvider.HospitalProvider
+}
+
+class HospitalReadOperations {
+  static DB = DatabaseProvider.HospitalProvider
+
+  static async fetchPendingDoctors(
+    hospitalId: number,
+  ): Promise<{
+    success: number
+    doctors: PreviewType[]
+    message: string
+  }> {
+    if (!Number.isInteger(hospitalId) || hospitalId <= 0) {
+      throw new HospitalError('Invalid or missing hospital id')
+    }
+
+    try {
+      const { hospital } = await this.DB.fetchHospital(hospitalId)
+      if (!hospital) {
+        throw new HospitalError("hospital doesn't exist")
+      }
+
+      const doctors = hospital.doctors.filter((doctor: PreviewType) => {
+        return doctor.status === ApprovalStatus.Pending
+      })
+
+      if (doctors.length === 0) {
+        return {
+          success: ErrorCodes.NotFound,
+          doctors: [],
+          message: 'No pending doctors found',
+        }
+      }
+
+      return {
+        success: ErrorCodes.Success,
+        doctors,
+        message: 'pending doctors found',
+      }
+    } catch (error) {
+      console.error(error)
+      throw new HospitalError('Error fetching pending doctors')
+    }
+  }
+
+  static async fetchPendingPharmacist(
+    hospitalId: number,
+  ): Promise<{
+    success: number
+    pharmacists: PreviewType[]
+    message: string
+  }> {
+    if (!Number.isInteger(hospitalId) || hospitalId <= 0) {
+      throw new HospitalError('Invalid or missing hospital id')
+    }
+
+    try {
+      const { hospital } = await this.DB.fetchHospital(hospitalId)
+      if (!hospital) {
+        throw new HospitalError("hospital doesn't exist")
+      }
+
+      const pharmacists = hospital.pharmacists.filter(
+        (pharmacist: PreviewType) => {
+          return pharmacist.status === ApprovalStatus.Pending
+        },
+      )
+
+      if (pharmacists.length === 0) {
+        return {
+          success: ErrorCodes.NotFound,
+          pharmacists: [],
+          message: 'No pending pharmacists found',
+        }
+      }
+
+      return {
+        success: ErrorCodes.Success,
+        pharmacists,
+        message: 'pending pharmacists found',
+      }
+    } catch (error) {
+      console.error(error)
+      throw new HospitalError('Error fetching pharmacists')
+    }
+  }
+}
+
+export class HospitalService {
+  static Helpers = HospitalHelpers
+  static Read = HospitalReadOperations
+  static DB = DatabaseProvider.HospitalProvider
+
+  static async getHospitalById(
+    id: number,
+  ): Promise<{ success: number; hospital: HospitalType }> {
+    if (!Number.isInteger(id) || id <= 0) {
+      throw new HospitalError('Invalid or missing hospital id')
+    }
+
+    try {
+      const hospital = await this.DB.fetchHospital(id)
+      if (!hospital) {
+        throw new HospitalError("hospital doesn't exist")
+      }
+
+      return {
+        success: 200,
+        hospital,
+      }
+    } catch (error) {
+      console.error(error)
+      throw new HospitalError('Error fetching hospital')
+    }
+  }
+
+  static async fetchPendingHospitals(): Promise<{
+    success: number
+    Hospital: HospitalType[]
+  }> {
+    try {
+      const Hospital = await this.DB.fetchHospitalWithPendingStatus()
+      if (Hospital.length === 0) {
+        return {
+          success: 200,
+          Hospital: [],
+        }
+      }
+      console.log(Hospital)
+      return {
+        success: 200,
+        Hospital,
+      }
+    } catch (error) {
+      console.error(error)
+      throw new HospitalError('Error fetching pending hospitals')
+    }
+  }
+
+  static async fetchApprovedHospitals(): Promise<{
+    success: number
+    hospitals: HospitalType[]
+  }> {
+    try {
+      const hospitals = await this.DB.fetchHospitalWithApprovedStatus()
+      if (!hospitals) {
+        console.log('No approved hospitals')
+        throw new HospitalError('No approved hospitals found')
+      }
+      if (hospitals.length === 0) {
+        return {
+          success: 404,
+          hospitals: [],
+        }
+      }
+
+      return {
+        success: 200,
+        hospitals,
+      }
+    } catch (error) {
+      console.error(error)
+      throw new HospitalError('Error fetching approved hospitals')
+    }
+  }
+}
