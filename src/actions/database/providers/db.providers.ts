14b6387faff4e1b3a7f39b7da6eaa4bc8efcb853
3illBaby <thrillanon@outlook.com>
2024-04-01 11:44:03 +0100

    chore(db.providers.ts): added Hospital provider

--- src/actions/database/providers/db.providers.ts
+++ src/actions/database/providers/db.providers.ts
@@ -1,7 +1,9 @@
 'use server'
 
 import { AdminDB } from '../admin/dao/admin.dao'
+import { HospitalDB } from '../hospital/dao/hospital.dao'
 
 export const DatabaseProvider = {
   AdminProvider: AdminDB,
+  HospitalProvider: HospitalDB,
 }
