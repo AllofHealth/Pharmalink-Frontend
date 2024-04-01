a877ca549994b1ea64e52cd0fefcda355835d280
3illBaby <thrillanon@outlook.com>
2024-04-01 12:17:42 +0100

    chore(db.provider.ts): updated provider with Doctor class

--- src/actions/database/providers/db.providers.ts
+++ src/actions/database/providers/db.providers.ts
@@ -2,8 +2,10 @@
 
 import { AdminDB } from '../admin/dao/admin.dao'
 import { HospitalDB } from '../hospital/dao/hospital.dao'
+import { DoctorDB } from '../doctor/dao/doctor.dao'
 
 export const DatabaseProvider = {
   AdminProvider: AdminDB,
   HospitalProvider: HospitalDB,
+  DoctorProvider: DoctorDB,
 }
