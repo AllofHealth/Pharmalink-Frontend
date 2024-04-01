7990b7eb314662da947386ed8aab2773da130f94
3illBaby <thrillanon@outlook.com>
2024-04-01 11:13:03 +0100

    chore(hospital.dao.ts): added database core functions

--- src/actions/database/hospital/dao/hospital.dao.ts
+++ src/actions/database/hospital/dao/hospital.dao.ts
@@ -1,1 +1,47 @@
 'use server'
+import { schemaProvider } from '@/actions/mongoose/schemas/schema_provider/schema.providers'
+import { ApprovalStatus, Category } from '@/actions/shared/global'
+import { CreateHospitalType } from '../interface/hospital.interface'
+
+export class HospitalDB {
+  static Hospital = schemaProvider.getSchema('Hospital')
+  static async createNewHospital(hospital: CreateHospitalType) {
+    return await this.Hospital.create({
+      id: hospital.id,
+      name: hospital.name,
+      admin: hospital.admin,
+      email: hospital.email,
+      phoneNo: hospital.phoneNo,
+      regNo: hospital.regNo,
+      location: hospital.location,
+      profilePicture: hospital.profilePicture ? hospital.profilePicture : '',
+      description: hospital.description,
+      status: ApprovalStatus.Pending,
+      category: Category.Hospital,
+    })
+  }
+
+  static async fetchHospital(id: number) {
+    return await this.Hospital.findOne({ id })
+  }
+
+  static async fetchHospitalByRegNo(regNo: string) {
+    return await this.Hospital.findOne({ regNo })
+  }
+
+  static async fetchAllHospitals() {
+    return await this.Hospital.find()
+  }
+
+  static async fetchHospitalWithPendingStatus() {
+    return await this.Hospital.find({
+      status: ApprovalStatus.Pending,
+    })
+  }
+
+  static async fetchHospitalWithApprovedStatus() {
+    return await this.Hospital.find({
+      status: ApprovalStatus.Approved,
+    })
+  }
+}
