567e3226d31b33b12d20b732b1f8832fc23c65b9
3illBaby <thrillanon@outlook.com>
2024-04-01 11:10:08 +0100

    refactor(schema.providers.ts): refactored schema provider

--- src/actions/mongoose/schemas/schema_provider/schema.providers.ts
+++ src/actions/mongoose/schemas/schema_provider/schema.providers.ts
@@ -5,11 +5,27 @@ import Doctor from '../doctor.schema'
 import Hospital from '../hospital.schema'
 import Pharmacist from '../pharmacist.schema'
 import Patient from '../patient.schema'
+import mongoose, { Schema } from 'mongoose'
 
-export const SchemaProvider = {
-  Admin: Admin,
-  Doctor: Doctor,
-  Hospital: Hospital,
-  Pharmacist: Pharmacist,
-  Patient: Patient,
+interface ISchema {
+  [key: string]: mongoose.Model<any, {}, {}, {}, any, any>
 }
+
+class SchemaProvider {
+  private schemas: ISchema
+
+  constructor() {
+    this.schemas = {
+      Admin,
+      Doctor,
+      Hospital,
+      Pharmacist,
+      Patient,
+    }
+  }
+  public getSchema(schemaName: string): any {
+    return this.schemas[schemaName]
+  }
+}
+
+export const schemaProvider = new SchemaProvider()
