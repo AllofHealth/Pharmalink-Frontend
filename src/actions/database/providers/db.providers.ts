b0f28677e5b880f071e3e57943cc14bdcda8656a
3illBaby <thrillanon@outlook.com>
2024-04-02 23:23:34 +0100

    chore(db.provider.ts): exposed Pharmacist provider

--- src/actions/database/providers/db.providers.ts
+++ src/actions/database/providers/db.providers.ts
@@ -3,9 +3,11 @@
 import { AdminDB } from '../admin/dao/admin.dao'
 import { HospitalDB } from '../hospital/dao/hospital.dao'
 import { DoctorDB } from '../doctor/dao/doctor.dao'
+import { PharmacistDB } from '../pharmacist/dao/pharmacist.dao'
 
 export const DatabaseProvider = {
   AdminProvider: AdminDB,
   HospitalProvider: HospitalDB,
   DoctorProvider: DoctorDB,
+  PharmacistProvider: PharmacistDB,
 }
