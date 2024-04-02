5074e71a52723df633f75adfc3c78ced0fb26057
3illBaby <thrillanon@outlook.com>
2024-04-02 23:23:02 +0100

    chore(pharmacist.dao.ts): added db operations for pharmacist

--- src/actions/database/pharmacist/dao/pharmacist.dao.ts
+++ src/actions/database/pharmacist/dao/pharmacist.dao.ts
@@ -1,1 +1,49 @@
 'use server'
+
+import { schemaProvider } from '@/actions/mongoose/schemas/schema_provider/schema.providers'
+import { ApprovalStatus } from '@/actions/shared/global'
+import { CreatePharmacistType } from '../interface/pharmacist.interface'
+
+export class PharmacistDB {
+  static Pharmacists = schemaProvider.getSchema('Pharmacist')
+
+  static async createNewPharmacist(pharmacist: CreatePharmacistType) {
+    return await this.Pharmacists.create({
+      id: pharmacist.id,
+      name: pharmacist.name,
+      email: pharmacist.email,
+      profilePicture: pharmacist.profilePicture
+        ? pharmacist.profilePicture
+        : '',
+      location: pharmacist.location,
+      phoneNumber: pharmacist.phoneNumber,
+      walletAddress: pharmacist.walletAddress,
+      regNo: pharmacist.regNo,
+      status: ApprovalStatus.Pending,
+    })
+  }
+
+  static async fetchPharmacist(id: number) {
+    return await this.Pharmacists.findOne({ id })
+  }
+
+  static async fetchPharmacistByAddress(address: string) {
+    return await this.Pharmacists.findOne({ walletAddress: address })
+  }
+
+  static async fetchAllPharmacist() {
+    return await this.Pharmacists.find()
+  }
+
+  static async fetchPharmacistWithPendingStatus() {
+    return await this.Pharmacists.find({ status: ApprovalStatus.Pending })
+  }
+
+  static async fetchPharmacistsWithApprovedStatus() {
+    return await this.Pharmacists.find({ status: ApprovalStatus.Approved })
+  }
+
+  static async removePharmacist(address: string) {
+    return await this.Pharmacists.deleteOne({ walletAddress: address })
+  }
+}
