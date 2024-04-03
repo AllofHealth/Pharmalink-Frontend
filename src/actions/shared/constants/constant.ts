a0ca05782db1e7f840eec7a0eca266a1da62fd02
3illBaby <thrillanon@outlook.com>
2024-04-03 23:30:43 +0100

    chore(constant.ts):added IPFS auth keys

--- src/actions/shared/constants/constant.ts
+++ src/actions/shared/constants/constant.ts
@@ -4,3 +4,7 @@ dotenv.config()
 
 export const CONTRACT_ADDRESS =
   process.env.CONTRACT_ADDRESS || '0x31fAe14A19e494f4B8eD2Ff2ea9FB2fc4B422cCA'
+export const IPFS_API_KEY =
+  process.env.IPFS_API_KEY || '2WJpI7hd9vAuxRx3fsVOP8MHmZg'
+export const IPFS_API_SECRET =
+  process.env.IPFS_API_SECRET || '77ba2d6ddc757ec9ede30775ea2b0a20'
