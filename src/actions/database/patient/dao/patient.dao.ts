40caa561f767143099ecf2fd5fc0834ff0b26eb5
3illBaby <thrillanon@outlook.com>
2024-04-10 16:02:03 +0100

    chore(patient.dao.ts):implemented database operations for patients

--- src/actions/database/patient/dao/patient.dao.ts
+++ src/actions/database/patient/dao/patient.dao.ts
@@ -1,1 +1,35 @@
 'use server'
+
+import { Category } from '@/actions/shared/global'
+import { CreatePatientType } from '../interface/patient.interface'
+import Patient from '@/actions/mongoose/schemas/patient.schema'
+
+export class PatientDB {
+  static async createNewPatient(patient: CreatePatientType) {
+    return await Patient.create({
+      id: patient.id,
+      appointmentCount: 0,
+      name: patient.name,
+      age: patient.age,
+      profilePicture: patient.profilePicture ? patient.profilePicture : '',
+      address: patient.address,
+      city: patient.city,
+      walletAddress: patient.walletAddress,
+      bloodGroup: patient.bloodGroup,
+      genotype: patient.genotype,
+      category: Category.Patient,
+    })
+  }
+
+  static async fetchPatientByAddress(walletAddress: string) {
+    return await Patient.findOne({ walletAddress })
+  }
+
+  static async fetchAllPatients() {
+    return await Patient.find()
+  }
+
+  static async DeletePatient(walletAddress: string) {
+    return await Patient.deleteOne({ walletAddress })
+  }
+}
