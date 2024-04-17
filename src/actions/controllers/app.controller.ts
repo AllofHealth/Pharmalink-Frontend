06511cb07fd9bd896640399c05164e32df9883c3
3illBaby <thrillanon@outlook.com>
2024-04-17 22:34:35 +0100

    chore(app.controller.ts):exposed controller to register a patient

--- src/actions/controllers/app.controller.ts
+++ src/actions/controllers/app.controller.ts
@@ -1,8 +1,15 @@
 'use server'
 
+import { RegisterPatient } from '../interfaces/Patient/app.patient.interface'
+import { SuperServicesProvider } from '../providers/services.providers'
+
 /**
  * @author 3illBaby
  * @description controller for all of health
  */
 
-export class AppControllers {}
+export class AppControllers {
+  async registerPatient(args: RegisterPatient) {
+    return SuperServicesProvider.SuperPatientService.createPatient(args)
+  }
+}
