b2deb86ba6897606dd6673fcc659f579e610df96
3illBaby <thrillanon@outlook.com>
2024-04-17 22:01:46 +0100

    chore(db.providers.ts):added database service providers

--- src/actions/database/providers/db.providers.ts
+++ src/actions/database/providers/db.providers.ts
@@ -5,6 +5,7 @@ import { HospitalDB } from '../hospital/dao/hospital.dao'
 import { DoctorDB } from '../doctor/dao/doctor.dao'
 import { PharmacistDB } from '../pharmacist/dao/pharmacist.dao'
 import { PatientDB } from '../patient/dao/patient.dao'
+import { PatientService } from '../patient/service/patient.service'
 
 export const DatabaseProvider = {
   AdminProvider: AdminDB,
@@ -12,4 +13,8 @@ export const DatabaseProvider = {
   DoctorProvider: DoctorDB,
   PharmacistProvider: PharmacistDB,
   PatientProvider: PatientDB,
+}
+
+export const DatabaseServiceProviders = {
+  PatientDatabaseServices: PatientService,
 }
