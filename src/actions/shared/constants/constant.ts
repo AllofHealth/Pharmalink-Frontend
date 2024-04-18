b346da35687cb22d0aeb5eee269d895412262ea3
3illBaby <thrillanon@outlook.com>
2024-04-18 16:05:03 +0100

    chore(constants.ts):redeployed contract and updated address

--- src/actions/shared/constants/constant.ts
+++ src/actions/shared/constants/constant.ts
@@ -3,7 +3,7 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 export const CONTRACT_ADDRESS =
-  process.env.CONTRACT_ADDRESS || '0x21FC1569767da5B6C0Ab72eDEFC610E92FC42d43'
+  process.env.CONTRACT_ADDRESS || '0x08cE200554d317bAD4d880C82E5fb21fA632a8de'
 export const IPFS_API_KEY =
   process.env.IPFS_API_KEY || '2WJpI7hd9vAuxRx3fsVOP8MHmZg'
 export const IPFS_API_SECRET =
