365e61324b6cfa60b13d9820380159ad2c46ea17
3illBaby <thrillanon@outlook.com>
2024-04-18 15:59:16 +0100

    chore(app.controller.ts):exposed endpoint to create doctor

--- src/actions/controllers/app.controller.ts
+++ src/actions/controllers/app.controller.ts
@@ -1,5 +1,6 @@
 'use server'
 
+import { RegisterDoctor } from '../interfaces/Doctor/app.doctor.interface'
 import { RegisterPatient } from '../interfaces/Patient/app.patient.interface'
 import { SuperServicesProvider } from '../providers/services.providers'
 
@@ -11,5 +12,9 @@ import { SuperServicesProvider } from '../providers/services.providers'
 export class AppControllers {
   async registerPatient(args: RegisterPatient) {
     return SuperServicesProvider.SuperPatientService.createPatient(args)
+  }
+
+  async registerDoctor(args: RegisterDoctor) {
+    return SuperServicesProvider.SuperDoctorService.createDoctor(args)
   }
 }
