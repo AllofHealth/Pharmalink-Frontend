bdced3f99c84b7e75682dd6f558aa0aa200691e0
3illBaby <thrillanon@outlook.com>
2024-04-18 16:02:18 +0100

    chore(services.providers.ts):exported superDoctorService

--- src/actions/providers/services.providers.ts
+++ src/actions/providers/services.providers.ts
@@ -1,7 +1,9 @@
 'use server'
 
+import { superDoctorService } from '../services/doctor.services'
 import { superPatientService } from '../services/patient.services'
 
 export const SuperServicesProvider = {
   SuperPatientService: superPatientService,
+  SuperDoctorService: superDoctorService,
 }
