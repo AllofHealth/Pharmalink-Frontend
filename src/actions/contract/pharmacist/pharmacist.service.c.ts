b667a35e15334ea016eff3e753a815fb34134ebe
3illBaby <thrillanon@outlook.com>
2024-04-19 20:49:33 +0100

    feat(pharmacist.service.c.ts):implemented contract function

--- src/actions/contract/pharmacist/pharmacist.service.c.ts
+++ src/actions/contract/pharmacist/pharmacist.service.c.ts
@@ -1,1 +1,45 @@
 'use server'
+import { processEvent } from '@/actions/shared/utils/EventLogger/event.processor'
+import { ContractProvider } from '../provider/contract.provider'
+import { string2Bytes32 } from '@/actions/shared/utils/bytes.utils'
+import {
+  ContractEvents,
+  ErrorCodes,
+  EventNames,
+  PharmacistError,
+} from '@/actions/shared/global'
+
+class Pharmacist {
+  async addPharmacist(
+    hospitalIds: number,
+    regNo: string,
+  ): Promise<{ success: number; pharmacistId: number }> {
+    try {
+      const contract = await ContractProvider.Factory().provideContract()
+      const signer = await ContractProvider.Factory().getSigner()
+      const address = await signer.getAddress()
+      const transaction = await contract.createPharmacist(
+        hospitalIds,
+        address,
+        string2Bytes32(regNo),
+      )
+
+      const receipt = await transaction.wait()
+      const eventResult = await processEvent(
+        receipt,
+        EventNames.PharmacistAdded,
+        ContractEvents.PharmacistAdded,
+      )
+
+      return {
+        success: ErrorCodes.Success,
+        pharmacistId: eventResult.pharmacistId.toNumber(),
+      }
+    } catch (error) {
+      console.error(error)
+      throw new PharmacistError('Error adding pharmacist to contract')
+    }
+  }
+}
+
+export const PharmacistService = new Pharmacist()
