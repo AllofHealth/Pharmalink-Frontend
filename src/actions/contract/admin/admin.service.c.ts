4461fe8f220b23f8912793964a3d79c6d1d1f77c
3illBaby <thrillanon@outlook.com>
2024-04-25 22:19:56 +0100

    feat(admin.service.c.ts):implemented contract call for admin

--- src/actions/contract/admin/admin.service.c.ts
+++ src/actions/contract/admin/admin.service.c.ts
@@ -1,1 +1,40 @@
 'use server'
+
+import { processEvent } from '@/actions/shared/utils/EventLogger/event.processor'
+import { ContractProvider } from '../provider/contract.provider'
+import {
+  AdminError,
+  ContractEvents,
+  ErrorCodes,
+  EventNames,
+} from '@/actions/shared/global'
+
+export class Admin {
+  private provider = ContractProvider.Factory()
+
+  async createSystemAdmin(
+    address: string,
+  ): Promise<{ success: number; adminId: number }> {
+    try {
+      const contract = await this.provider.provideContract()
+      const transaction = await contract.addSystemAdmin(address)
+
+      const receipt = await transaction.wait()
+      const eventResult = await processEvent(
+        receipt,
+        EventNames.SystemAdminAdded,
+        ContractEvents.SystemAdminAdded,
+      )
+
+      return {
+        success: ErrorCodes.Success,
+        adminId: eventResult.adminId.toNumber(),
+      }
+    } catch (error) {
+      console.error(error)
+      throw new AdminError('Error adding admin to contract')
+    }
+  }
+}
+
+export const adminService = new Admin()
