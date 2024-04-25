684d6dd0cee826242b1e235651378e23f0d76c1d
3illBaby <thrillanon@outlook.com>
2024-04-25 20:27:25 +0100

    feat(hospital.service.c.ts): implemented contract call to register hospital

--- src/actions/contract/hospital/hospital.service.c.ts
+++ src/actions/contract/hospital/hospital.service.c.ts
@@ -1,1 +1,41 @@
 'use server'
+
+import { bytes32ToString } from '@/actions/shared/utils/bytes.utils'
+import { ContractProvider } from '../provider/contract.provider'
+import { processEvent } from '@/actions/shared/utils/EventLogger/event.processor'
+import {
+  ContractEvents,
+  ErrorCodes,
+  EventNames,
+  HospitalError,
+} from '@/actions/shared/global'
+
+class Hospital {
+  private provider = ContractProvider.Factory()
+  async createHospital(
+    regNo: string,
+  ): Promise<{ success: number; hospitalId: number }> {
+    try {
+      const contract = await this.provider.provideContract()
+      const transaction = await contract.createHospital(bytes32ToString(regNo))
+
+      const receipt = await transaction.wait()
+
+      const eventResult = await processEvent(
+        receipt,
+        EventNames.HospitalCreated,
+        ContractEvents.HospitalCreated,
+      )
+
+      return {
+        success: ErrorCodes.Success,
+        hospitalId: eventResult.hospitalId.toNumber(),
+      }
+    } catch (error) {
+      console.error(error)
+      throw new HospitalError('Error creating hospital')
+    }
+  }
+}
+
+export const HospitalService = new Hospital()
