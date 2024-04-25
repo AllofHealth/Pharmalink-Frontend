12aebd58e5c70a74f3805a24543bec40d2801145
3illBaby <thrillanon@outlook.com>
2024-04-25 21:37:18 +0100

    chore(constants.ts):added placeholder for profiles

--- src/actions/shared/constants/constant.ts
+++ src/actions/shared/constants/constant.ts
@@ -8,3 +8,7 @@ export const IPFS_API_KEY =
   process.env.IPFS_API_KEY || '2WJpI7hd9vAuxRx3fsVOP8MHmZg'
 export const IPFS_API_SECRET =
   process.env.IPFS_API_SECRET || '77ba2d6ddc757ec9ede30775ea2b0a20'
+export const PROFILE_PLACEHOLDER =
+  'https://www.kindpng.com/picc/m/722-7221920_placeholder-profile-image-placeholder-png-transparent-png.png'
+export const HOSPITAL_PLACEHOLDER =
+  'https://www.kindpng.com/picc/m/264-2646273_hydrodent-micro-channel-icon-hospital-navigation-icon-hospital.png'
