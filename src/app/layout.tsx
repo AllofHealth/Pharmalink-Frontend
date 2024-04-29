43a3338b1bcaebb36a734e1ed5a2dcbfcb916e70
Amzat19 <kunmiamzat1@gmail.com>
2024-04-29 18:16:42 +0100

    Reconfigured to use rainbowkit for connect wallet integration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,8 @@ import "./globals.css";
 import { Provider } from "react-redux";
 import { configureStore } from "@reduxjs/toolkit";
 import rootReducer from "@/lib/redux/rootReducer";
-import { Web3Modal } from "@/lib/context/web3Modal";
+import "@rainbow-me/rainbowkit/styles.css";
+import { Providers } from "./providers";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -22,7 +23,7 @@ export default function RootLayout({
     <Provider store={store}>
       <html lang="en">
         <body className={inter.className}>
-          <Web3Modal>{children}</Web3Modal>
+          <Providers>{children}</Providers>
         </body>
       </html>
     </Provider>
---
+++ src/app/providers.tsx
@@ -0,0 +1,58 @@
+import {
+  RainbowKitProvider,
+  getDefaultWallets,
+  Locale,
+  getDefaultConfig,
+} from "@rainbow-me/rainbowkit";
+import {
+  argentWallet,
+  trustWallet,
+  ledgerWallet,
+} from "@rainbow-me/rainbowkit/wallets";
+import { WagmiProvider } from "wagmi";
+import {
+  arbitrum,
+  base,
+  mainnet,
+  optimism,
+  polygon,
+  sepolia,
+} from "wagmi/chains";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import type React from "react";
+import type { ReactNode } from "react";
+
+const { wallets } = getDefaultWallets();
+
+const config = getDefaultConfig({
+  appName: "RainbowKit demo",
+  projectId: "27414159f22891f65dac78f285165179",
+  wallets: [
+    ...wallets,
+    {
+      groupName: "Other",
+      wallets: [argentWallet, trustWallet, ledgerWallet],
+    },
+  ],
+  chains: [
+    mainnet,
+    polygon,
+    optimism,
+    arbitrum,
+    base,
+    ...(process.env.NEXT_PUBLIC_ENABLE_TESTNETS === "true" ? [sepolia] : []),
+  ],
+  ssr: true,
+});
+
+const queryClient = new QueryClient();
+
+export function Providers({ children }: { children: ReactNode }) {
+  return (
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <RainbowKitProvider>{children}</RainbowKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
+  );
+}
