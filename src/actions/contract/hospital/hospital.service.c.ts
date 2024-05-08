0c2ca929dfe87f85695b2c13e2245b1dc7e92e83
3illBaby <thrillanon@outlook.com>
2024-05-08 21:19:17 +0100

    feat(hospital.service.c.ts):implemented function to approve pharmacist and doctor

--- src/actions/contract/hospital/hospital.service.c.ts
+++ src/actions/contract/hospital/hospital.service.c.ts
@@ -7,6 +7,14 @@ import {
   EventNames,
   HospitalError,
 } from '@/actions/shared/global'
+import {
+  ApproveDoctorType,
+  ApprovePharmacistType,
+} from '@/actions/interfaces/Doctor/app.doctor.interface'
+
+/**
+ * todo: implement high order function to determine role of wallet address and serve the correct function to approve practitioner
+ */
 
 async function createHospital(
   regNo: string,
@@ -25,7 +33,7 @@ async function createHospital(
 
     return {
       success: ErrorCodes.Success,
-      hospitalId: eventResult.hospitalId.toNumber(),
+      hospitalId: eventResult.hospitalId,
     }
   } catch (error) {
     console.error(error)
@@ -33,4 +41,66 @@ async function createHospital(
   }
 }
 
-export default createHospital
+async function approveDoctor(args: ApproveDoctorType) {
+  try {
+    const { doctorAddress, doctorId, hospitalId, regNo } = args
+    const contract = await provideContract()
+    const transaction = await contract.approveDoctor(
+      doctorAddress,
+      hospitalId,
+      doctorId,
+      bytes32ToString(regNo),
+    )
+
+    const receipt = await transaction.wait()
+    const eventResult = await processEvent(
+      receipt,
+      EventNames.DoctorApproved,
+      ContractEvents.DoctorApproved,
+    )
+
+    return {
+      success: ErrorCodes.Success,
+      doctorId: eventResult.doctorId,
+      hospitalId: eventResult.hospitalId,
+      doctorAddress: eventResult.doctor,
+    }
+  } catch (error) {
+    console.error(error)
+    throw new HospitalError('Error approving doctor')
+  }
+}
+
+async function approvePharmacist(args: ApprovePharmacistType) {
+  try {
+    const { pharmacistAddress, hospitalId, pharmacistId, regNo } = args
+    const contract = await provideContract()
+    const transaction = await contract.approvePharmacist(
+      pharmacistAddress,
+      hospitalId,
+      pharmacistId,
+      bytes32ToString(regNo),
+    )
+
+    const receipt = await transaction.wait()
+    const eventResult = await processEvent(
+      receipt,
+      EventNames.PharmacistApproved,
+      ContractEvents.PharmacistApproved,
+    )
+
+    return {
+      success: ErrorCodes.Success,
+      pharmacistId: eventResult.pharmacistId,
+      hospitalId: eventResult.hospitalId,
+      pharmacistAddress: eventResult.pharmacist,
+    }
+  } catch (error) {
+    console.error(error)
+    throw new HospitalError('Error approving pharmacist')
+  }
+}
+
+async function approvePractitioner() {}
+
+export { createHospital, approveDoctor, approvePharmacist }
