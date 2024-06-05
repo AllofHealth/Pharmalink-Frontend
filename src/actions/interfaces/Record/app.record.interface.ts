83ecdd8ca12c985ef7a49bb34a69cdcbc649b358
3illBaby <thrillanon@outlook.com>
2024-06-05 08:47:34 +0100

    chore(app.record.interface):added  interface to upload record to ipfs

---
+++ src/actions/interfaces/Record/app.record.interface.ts
@@ -0,0 +1,11 @@
+export interface RecordInterface {
+  diagnosis: string
+  content: string
+  doctorsName: string
+  hospitalName: string
+  date: Date
+}
+
+export interface UploadImageInterface {
+  image: File
+}
