80cb1793d878ac4ae9842505409c4754977dfe29
3illBaby <thrillanon@outlook.com>
2024-06-05 08:49:47 +0100

    chore(constants.ts):added new contract address

--- src/actions/shared/constants/constant.ts
+++ src/actions/shared/constants/constant.ts
@@ -2,10 +2,10 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 export const CONTRACT_ADDRESS =
-  process.env.CONTRACT_ADDRESS || '0x891525d01dc10b6690f5DE2200d28B18864FE187'
+  process.env.CONTRACT_ADDRESS || '0x5f25C3a063A78eE9aF4E7284Ed82554d774c2388'
 export const IPFS_API_KEY =
-  process.env.IPFS_API_KEY || '2WJpI7hd9vAuxRx3fsVOP8MHmZg'
+  process.env.IPFS_API_KEY || '3a7f7aa0e3864e3c97172d8ebabd12c0'
 export const IPFS_API_SECRET =
-  process.env.IPFS_API_SECRET || '77ba2d6ddc757ec9ede30775ea2b0a20'
+  process.env.IPFS_API_SECRET || '558fc131c6f94df5b242b9d17442014f'
 export const PROJECT_ID =
   process.env.PROJECT_ID || '27414159f22891f65dac78f285165179'
