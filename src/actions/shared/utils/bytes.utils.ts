8efeca37a97f0de153ce81c58f36e77df7ae18a4
3illBaby <thrillanon@outlook.com>
2024-06-05 08:50:26 +0100

    chore(bytes.utils.ts):added utility function to return empty bytes

--- src/actions/shared/utils/bytes.utils.ts
+++ src/actions/shared/utils/bytes.utils.ts
@@ -1,11 +1,13 @@
-'use server'
-
 import { ethers } from 'ethers'
 
-export const string2Bytes32 = (str: string): string => {
+export const string2Bytes32 = (str: string) => {
   return ethers.encodeBytes32String(str)
 }
 
-export const bytes32ToString = (bytes32: string): string => {
+export const bytes32ToString = (bytes32: string) => {
   return ethers.decodeBytes32String(bytes32)
+}
+
+export const getEmptyBytes32 = () => {
+  return ethers.ZeroHash
 }
