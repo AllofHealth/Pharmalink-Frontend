96ff51f63c28845da2bb4de44ccab8b06b27c458
3illBaby <thrillanon@outlook.com>
2024-06-19 00:10:27 +0100

    refactor(constants.ts):added new contract address

--- src/actions/shared/constants/constant.ts
+++ src/actions/shared/constants/constant.ts
@@ -2,7 +2,7 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 export const CONTRACT_ADDRESS =
-  process.env.CONTRACT_ADDRESS || '0x5f25C3a063A78eE9aF4E7284Ed82554d774c2388'
+  process.env.CONTRACT_ADDRESS || '0xd1345513C56d5925f4c3d614Af0bf5bd7dA4cba1'
 export const IPFS_API_KEY =
   process.env.IPFS_API_KEY || '3a7f7aa0e3864e3c97172d8ebabd12c0'
 export const IPFS_API_SECRET =
