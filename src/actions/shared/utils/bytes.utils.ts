c367de186eb6434062caf8545c230f176f7ab278
3illBaby <thrillanon@outlook.com>
2024-09-15 21:24:43 +0100

    Refactor bytes utility functions into a class
    Reorganize the bytes utility functions by encapsulating them within a `BytesUtils` class. This change improves the modularity and maintainability of the codebase by grouping related functions together.

--- src/actions/shared/utils/bytes.utils.ts
+++ src/actions/shared/utils/bytes.utils.ts
@@ -1,13 +1,11 @@
 import { ethers } from 'ethers'
 
-export const string2Bytes32 = (str: string) => {
-  return ethers.encodeBytes32String(str)
-}
+export class BytesUtils {
+  string2Bytes(str: string): string {
+    return  ethers.encodeBytes32String(str);
+  }
 
-export const bytes32ToString = (bytes32: string) => {
-  return ethers.decodeBytes32String(bytes32)
-}
-
-export const getEmptyBytes32 = () => {
-  return ethers.ZeroHash
+  bytes32ToString(str: string): string {
+    return ethers.encodeBytes32String(str);
+  }
 }
