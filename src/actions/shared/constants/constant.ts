6bd421adc2709d3d087714411ab355432e6cc0c6
3illBaby <thrillanon@outlook.com>
2024-09-18 23:16:38 +0100

    Update contract address and format environment variable exports
    Updated the CONTRACT_ADDRESS constant with a new Ethereum address. Adjusted formatting for environment variable fallbacks to improve code readability.

--- src/actions/shared/constants/constant.ts
+++ src/actions/shared/constants/constant.ts
@@ -1,10 +1,11 @@
 import dotenv from 'dotenv'
+
 dotenv.config()
 
-export const CONTRACT_ADDRESS = '0x92aAc9DE16B8F9f862c1dbC21F9c0aa3cbdd3c03'
+export const CONTRACT_ADDRESS = '0x275e56A13eF2872B6069DA0Fef5Aa14AA3535726';
 export const IPFS_API_KEY =
-  process.env.IPFS_API_KEY || '3a7f7aa0e3864e3c97172d8ebabd12c0'
+    process.env.IPFS_API_KEY || '3a7f7aa0e3864e3c97172d8ebabd12c0'
 export const IPFS_API_SECRET =
-  process.env.IPFS_API_SECRET || '558fc131c6f94df5b242b9d17442014f'
+    process.env.IPFS_API_SECRET || '558fc131c6f94df5b242b9d17442014f'
 export const PROJECT_ID =
-  process.env.PROJECT_ID || '27414159f22891f65dac78f285165179'
+    process.env.PROJECT_ID || '27414159f22891f65dac78f285165179'
