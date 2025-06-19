8e66dd6667356182765962dbca133845b6b433d0
3illBaby <thrillanon@outlook.com>
2025-06-19 14:13:16 +0100

    chore(constants.ts): cleaned

--- src/actions/shared/constants/constant.ts
+++ src/actions/shared/constants/constant.ts
@@ -3,9 +3,6 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const CONTRACT_ADDRESS = "0x9799A202eaB37666c81F643c9c4b731b05433f33";
-export const IPFS_API_KEY =
-  process.env.IPFS_API_KEY || "3a7f7aa0e3864e3c97172d8ebabd12c0";
-export const IPFS_API_SECRET =
-  process.env.IPFS_API_SECRET || "558fc131c6f94df5b242b9d17442014f";
-export const PROJECT_ID =
-  process.env.PROJECT_ID || "27414159f22891f65dac78f285165179";
+export const IPFS_API_KEY = process.env.IPFS_API_KEY || "";
+export const IPFS_API_SECRET = process.env.IPFS_API_SECRET || "";
+export const PROJECT_ID = process.env.PROJECT_ID || "";
